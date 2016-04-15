1d114fcc48f47625466fe75cec0fc2ce06853a70
Ellim <seiferttyrindal@gmail.com>
2016-04-15 08:44:42 -0600

    Update main.js

--- js/main.js
+++ js/main.js
@@ -3,9 +3,9 @@ function init(){
 	var modtit = document.getElementById("mtitle")
 	modtit.innerHTML = ""
 	modtit.innerHTML = "Abyss"
-    var divid = document.getElementById("speedwalkdiv")
-    divid.innerHTML = ""
-    divid.innerHTML = "3w;open west;11ws2w2sws7w3swd<br /><span class='text-primary'>ue3n7ene2n2en10e;open east;4e</span>"
+    var divid2 = document.getElementById("speedwalkdiv")
+    divid2.innerHTML = ""
+    divid2.innerHTML = "3w;open west;11ws2w2sws7w3swd<br /><span class='text-primary'>ue3n7ene2n2en10e;open east;4e</span>"
   }
   $('.selectpicker').on('change', function(){
     var selected = $(this).find("option:selected").val();
