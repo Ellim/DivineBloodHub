2f730528a0edfb8a4f67c1852b0cd4b2194dd6ab
Ellim <seiferttyrindal@gmail.com>
2016-04-15 10:26:43 -0600

    Update main.js

--- js/main.js
+++ js/main.js
@@ -1,14 +1,7 @@
 function init(){
-  document.getElementById("modaltest").onclick = function() {
-	var modtit = document.getElementById("mtitle")
-	modtit.innerHTML = ""
-	modtit.innerHTML = "Abyss"
-    var divid2 = document.getElementById("speedwalkdiv")
-    divid2.innerHTML = ""
-    divid2.innerHTML = "3w;open west;11ws2w2sws7w3swd<br /><span class='text-primary'>ue3n7ene2n2en10e;open east;4e</span>"
-  }
   $('.selectpicker').on('change', function(){
     var selected = $(this).find("option:selected").val();
+    alert(selected)
     updateWell(selected);
   });
 }
@@ -29,4 +22,3 @@ function updateWell(area) {
 			break;
 	}
 }
-
