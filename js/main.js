343dde546976234e26ee3e4b1984fa7ac87b1584
Ellim <seiferttyrindal@gmail.com>
2016-04-15 11:31:41 -0600

    Update main.js

--- js/main.js
+++ js/main.js
@@ -19,5 +19,15 @@ function updateWell(area) {
 			divid.innerHTML = ""
 			divid.innerHTML = "3e;open east;4e4s2es2ed;open west;ws3dn;open down<br /><span class='text-primary'>s3un;open east;eu2wn2w4n3w;open west;4w</span>"
 			break;
+		case "Amongst the Clouds":
+			var divid = document.getElementById("swalk");
+			divid.innerHTML = ""
+			divid.innerHTML = "6sws2w4se4us<br /><span class='text-primary'>n4dw4n2ene6n</span>"
+			break;
+		case "Amyshor Woods":
+			var divid = document.getElementById("swalk");
+			divid.innerHTML = ""
+			divid.innerHTML = "3w;open west;11ws2w2sws10ws;SW<br /><span class='text-primary'>NE;n10ene2n2en10e;open east;4e</span>"
+			break;
 	}
 }
