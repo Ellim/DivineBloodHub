08f167fc284dcfba5f1a8266814258110f25425d
Ellim <seiferttyrindal@gmail.com>
2016-04-15 06:52:23 -0600

    updates

--- js/main.js
+++ js/main.js
@@ -1,13 +1,11 @@
 function init(){
   document.getElementById("modaltest").onclick = function() {
-    var divid = document.getElementById("speedwalkdiv");
+	var modtit = document.getElementById("mtitle")
+	modtit.innerHTML = ""
+	modtit.innerHTML = "Abyss"
+    var divid = document.getElementById("speedwalkdiv")
     divid.innerHTML = ""
-    divid.innerHTML = "7e4s2es2eds"
-  }
-  document.getElementById("abysswellbutton").onclick = function() {
-    if ($('#abysswell').hasClass('hidden')) {
-		$('#abysswell').removeClass('hidden');
-	  }
+    divid.innerHTML = "3w;open west;11ws2w2sws7w3swd<br /><span class='text-primary'>ue3n7ene2n2en10e;open east;4e</span>"
   }
 }
 window.onload=init;
