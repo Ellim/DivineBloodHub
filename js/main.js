a3d3ea776d98a34d50f2648e4544a4ebec74732c
Ellim <seiferttyrindal@gmail.com>
2016-04-15 21:11:53 -0600

    speedwalks done

--- js/main.js
+++ js/main.js
@@ -219,5 +219,210 @@ function updateWell(area) {
 			divid.innerHTML = ""
 			divid.innerHTML = "3e;open east;4e4s2es2ed3ne4swse3sd6s;SE<br /><span class='text-primary'>NW;6nu3nwne4nw3su2wn2w4n3w;open west;4w</span>"
 			break;
+		case "In the Air":
+			var divid = document.getElementById("swalk");
+			divid.innerHTML = ""
+			divid.innerHTML = "<span class='text-success'>nu</span><br /><span class='text-primary'>ds</span>"
+			break;
+		case "Kaladar":
+			var divid = document.getElementById("swalk");
+			divid.innerHTML = ""
+			divid.innerHTML = "3w9s2e2sw3s2wswn<br /><span class='text-primary'>sen2e3ne2n2w9n3e</span><br />You will need to <span class='text-success'>look</span> for a room named 'Inside the Tavern'. Don't worry it'll be somewhere, just keep moving and <span class='text-success'>looking</span>!"
+			break;
+		case "Labyrinth, The":
+			var divid = document.getElementById("swalk");
+			divid.innerHTML = ""
+			divid.innerHTML = "3w;open west;11ws2w2sws10w2n2w6n;NE;3es2e3n<br /><span class='text-primary'>3s2wn3w;SW;6s2e2s10ene2n2en10e;open east;4e</span>"
+			break;
+		case "Land of the Druid":
+			var divid = document.getElementById("swalk");
+			divid.innerHTML = ""
+			divid.innerHTML = "3w;open west;11w2ne;open east;eds;open east;3e2nendn4w;enter p<br /><span class='text-primary'>enter p;4esusw2s2w;open west;wnuw;open west;w2s10e;open east;4e</span><br />There will be a Bullfrog in a lake to the north and west. Kill him and you'll save a faerie; take the faerie east of the portal and you will be allowed in to the Land of the Druid."
+			break;
+		case "Land of Incantation":
+			var divid = document.getElementById("swalk");
+			divid.innerHTML = ""
+			divid.innerHTML = "3e;open east;15e;open east;6e6n;open north;11nwn<br /><span class='text-primary'>se10s;open south;7s5w;open west;15w;open west;4w</span>"
+			break;
+		case "Land of Incantation (Elven Fortress)":
+			var divid = document.getElementById("swalk");
+			divid.innerHTML = ""
+			divid.innerHTML = "3e;open east;15e;open east;6e6n;open north;6n3e;open east;e<br /><span class='text-primary'>open west;4w5s;open south;7s5w;open west;15w;open west;4w</span>"
+			break;
+		case "Lord Aviston's Manor":
+			var divid = document.getElementById("swalk");
+			divid.innerHTML = ""
+			divid.innerHTML = "3w;open west;8w2nw6n2w2n<br /><span class='text-primary'>2s2e6se2s7e;open east;4e</span>"
+			break;
+		case "Mahn-Tor":
+			var divid = document.getElementById("swalk");
+			divid.innerHTML = ""
+			divid.innerHTML = "3e;open east;4e4s2es2ed3ne4swse3s<br /><span class='text-primary'>3nwne4nw3su2wn2w4n3w;open west;4w</span>"
+			break;
+		case "Merchant's Fair":
+			var divid = document.getElementById("swalk");
+			divid.innerHTML = ""
+			divid.innerHTML = "3w4n2e3n2w;NW;2w<br /><span class='text-primary'>2e;SE;2e3s2w4s3e</span>"
+			break;
+		case "Miden'Nir":
+			var divid = document.getElementById("swalk");
+			divid.innerHTML = ""
+			divid.innerHTML = "3w9s2e2s<br /><span class='text-primary'>2n2w9n3e</span>"
+			break;
+		case "Midgard Residential":
+			var divid = document.getElementById("swalk");
+			divid.innerHTML = ""
+			divid.innerHTML = "2sw<br /><span class='text-primary'>e2n</span>"
+			break;
+		case "Mongrel Hideout":
+			var divid = document.getElementById("swalk");
+			divid.innerHTML = ""
+			divid.innerHTML = "2sds4e2ned<br /><span class='text-primary'>uw2s4wnu2n</span>"
+			break;
+		case "Moria":
+			var divid = document.getElementById("swalk");
+			divid.innerHTML = ""
+			divid.innerHTML = "3e;open east;4e6n<br /><span class='text-primary'>6s3w;open west;4w</span>"
+			break;
+		case "New Ofcol":
+			var divid = document.getElementById("swalk");
+			divid.innerHTML = ""
+			divid.innerHTML = "3e4n2w7n6e2ne;NE;n<br /><span class='text-primary'>s;SW;w2s6w7s2w4s3e</span>"
+			break;
+		case "New Thalos":
+			var divid = document.getElementById("swalk");
+			divid.innerHTML = ""
+			divid.innerHTML = "3e;open east;15e;open east;6e<br /><span class='text-primary'>5w;open west;15w;open west;4w</span>"
+			break;
+		case "Ofcol":
+			var divid = document.getElementById("swalk");
+			divid.innerHTML = ""
+			divid.innerHTML = "3w4n2e7n6e4n<br /><span class='text-primary'>4s6w7s3w4s3e</span>"
+			break;
+		case "Old Thalos":
+			var divid = document.getElementById("swalk");
+			divid.innerHTML = ""
+			divid.innerHTML = "3e;open east;6e2n3e2n<br /><span class='text-primary'>s3w2s5w;open west;4w</span><br />You will need to <span class='text-success'>look</span> for a room named 'A Clearing in the Woods'. Don't worry it'll be somewhere, just keep moving and <span class='text-success'>looking</span>!"
+			break;
+		case "Orc Stronghold":
+			var divid = document.getElementById("swalk");
+			divid.innerHTML = ""
+			divid.innerHTML = "3w;open west;11ws2w2sws3wnw4n<br /><span class='text-primary'>4ses3ene2n2en10e;open east;4e</span>"
+			break;
+		case "Northern Plains":
+			var divid = document.getElementById("swalk");
+			divid.innerHTML = ""
+			divid.innerHTML = "3w4n2e3n<br /><span class='text-primary'>3s2w4s3e</span>"
+			break;
+		case "Quarterstaff":
+			var divid = document.getElementById("swalk");
+			divid.innerHTML = ""
+			divid.innerHTML = "3w;open west;11ws2w2sws10w2n2w13n<br /><span class='text-primary'>13s2e2s10ene2n2en10e;open east;4e</span>"
+			break;
+		case "Realm of Djinnin":
+			var divid = document.getElementById("swalk");
+			divid.innerHTML = ""
+			divid.innerHTML = "3e;open east;11e9s11e6s4es;SE;u<br /><span class='text-primary'>Xyliacai laughs mercilessly.</span>"
+			break;
+		case "Redelof":
+			var divid = document.getElementById("swalk");
+			divid.innerHTML = ""
+			divid.innerHTML = "3e;open east;15e;open east;12e;open east;5en;NE<br /><span class='text-primary'>SW;s4w;open west;12w;open west;15w;open west;4w</span>"
+			break;
+		case "Revelton":
+			var divid = document.getElementById("swalk");
+			divid.innerHTML = ""
+			divid.innerHTML = "<span class='text-success'>3e;open east;15e;open east;12e;open east;9e3s2e2s2es3e2n2enen13e;open east;3e5nw4n;open north;n</span><br /><span class='text-primary'>open south;5se5s2w;open west;14wsws2w2s3wn3wn2w2n3w3n7w;open west;12w;open west;15w;open west;4w</span>"
+			break;
+		case "Ruined Castle":
+			var divid = document.getElementById("swalk");
+			divid.innerHTML = ""
+			divid.innerHTML = "3e4n2w7n6e2ne;NE;5se2s2eu2e;SE;e3n<br /><span class='text-primary'>3sw;NW;2wd2w2nw5n;SW;w2s6w7s2e4s3w</span>"
+			break;
+		case "Sanctuary of Terror":
+			var divid = document.getElementById("swalk");
+			divid.innerHTML = ""
+			divid.innerHTML = "3e;open east;4e4s2es2ed;open west;ws3d2n<br /><span class='text-primary'>2s3un;open east;eu2wn2w4n3w;open west;4w</span>"
+			break;
+		case "Sands of Sorrow":
+			var divid = document.getElementById("swalk");
+			divid.innerHTML = ""
+			divid.innerHTML = "<span class='text-success'>s2e2s12e</span><br /><span class='text-primary'>12w2n2wn</span>"
+			break;
+		case "Sewers":
+			var divid = document.getElementById("swalk");
+			divid.innerHTML = ""
+			divid.innerHTML = "2sd<br /><span class='text-primary'>u2n</span>"
+			break;
+		case "Shadow Cult":
+			var divid = document.getElementById("swalk");
+			divid.innerHTML = ""
+			divid.innerHTML = "3w;open west;11ws2w2sws10w2n2w28n3w3n2w2n;open east;2e<br /><span class='text-primary'>w;open west;w2s2e3s3e28s2e2s10ene2n2en10e;open east;4e</span>"
+			break;
+		case "Shire":
+			var divid = document.getElementById("swalk");
+			divid.innerHTML = ""
+			divid.innerHTML = "3w;open west;3w4n<br /><span class='text-primary'>4s2e;open east;4e</span>"
+			break;
+		case "Smash Mountain":
+			var divid = document.getElementById("swalk");
+			divid.innerHTML = ""
+			divid.innerHTML = "3w;open west;11ws2w2sws3wnw4ne4n;NE;2e2nw4n<br /><span class='text-primary'>4se2s2w;SW;4sw4ses3ene2n2en10e;open east;4e</span>"
+			break;
+		case "Stones of G'Harne":
+			var divid = document.getElementById("swalk");
+			divid.innerHTML = ""
+			divid.innerHTML = "3e4n2w7n8e2se2s;SW;2w;SW;sd<br /><span class='text-primary'>un;NE;2e;NE;2nw2n8w7s2e4s3w</span>"
+			break;
+		case "Temple of Heroes":
+			var divid = document.getElementById("swalk");
+			divid.innerHTML = ""
+			divid.innerHTML = "3e;open east;6e2ne5ne5n;op north<br /><span class='text-primary'>5sw5sw2s5w;open west;4w</span>"
+			break;
+		case "Temple of Kyre":
+			var divid = document.getElementById("swalk");
+			divid.innerHTML = ""
+			divid.innerHTML = "3e;open east;11e9s11eswsw2s;open down;3d3n2w5nw;open down;2d<br /><span class='text-primary'>u;open up;ue5s2e3s2u;open up;u2nenen11w9n10w;open west;4w</span>"
+			break;
+		case "Temple of Villains":
+			var divid = document.getElementById("swalk");
+			divid.innerHTML = ""
+			divid.innerHTML = "<span class='text-success'>3w;open west;11ws2w2sw8s3e2s2w4n</span><br /><span class='text-primary'>4s2e2n3w8ne2n2en10e;open east;4e</span>"
+			break;
+		case "Tomb of the Ancients":
+			var divid = document.getElementById("swalk");
+			divid.innerHTML = ""
+			divid.innerHTML = "3e;open east;4e4s2es2ed5n4w2s;open down;d<br /><span class='text-primary'>open up;u2n4e5su2wn2wn2w4n3w;open west;4w</span>"
+			break;
+		case "Ruined Thalos":
+			var divid = document.getElementById("swalk");
+			divid.innerHTML = ""
+			divid.innerHTML = "3e;open east;4e4s4w<br /><span class='text-primary'>4e4n3w;open west;4w</span>"
+			break;
+		case "Troll Den":
+			var divid = document.getElementById("swalk");
+			divid.innerHTML = ""
+			divid.innerHTML = "s2e2s6wnwn3w2s;SW;2ws3w4s2w<br /><span class='text-primary'>2e4n3en2e;NE;2n3eses6e2n2wn</span>"
+			break;
+		case "Underground Shadow":
+			var divid = document.getElementById("swalk");
+			divid.innerHTML = ""
+			divid.innerHTML = "3e;open east;11e9s11eswsw2s;open down;d<br /><span class='text-primary'>open up;u2nenen11w9n10w;open west;4w</span>"
+			break;
+		case "Valley of the Elves":
+			var divid = document.getElementById("swalk");
+			divid.innerHTML = ""
+			divid.innerHTML = "3w4n2e6nw3n<br /><span class='text-primary'>3se6s2w4s3e</span>"
+			break;
+		case "Village of the Wegliks":
+			var divid = document.getElementById("swalk");
+			divid.innerHTML = ""
+			divid.innerHTML = "3e;open east;4e4s2es2ed3ne3sesd3es3en2w;NW;3ne2n<br /><span class='text-primary'>2sw3s;SE;2es3wn3wunw3nw3su2wn2w4n3w;open west;4w</span>"
+			break;
+		case "Wyvern's Tower":
+			var divid = document.getElementById("swalk");
+			divid.innerHTML = ""
+			divid.innerHTML = "<span class='text-success'>3e;open east;4e4s2es2ed8e</span><br /><span class='text-primary'>8wu2wn2w4n3w;open west;4w</span>"
+			break;
 	}
 }
